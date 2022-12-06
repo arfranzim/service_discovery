b4a6243aff6c9064aec63949182feca56f4997d7
arfranzim <andre.franzim@gmail.com>
2022-12-06 09:22:56 -0300

    Initial commit

---
+++ src/test/java/br/com/franzim/service_discovery/MsServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franzim.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
